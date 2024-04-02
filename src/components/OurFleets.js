4fdd0b718b55beda764954dbab4cb24c1f5bc60b
Gaurav <gauravhpanchal2@gmail.com>
2024-04-02 21:29:34 +0530

    what we offer added

--- src/components/OurFleets.js
+++ src/components/OurFleets.js
@@ -6,6 +6,7 @@ import carens from "@/images/carens.png";
 import innova from "@/images/innova.png";
 import ertiga from "@/images/ertiga.png";
 import tempoTraveller from "@/images/tempoTraveller.png";
+import Link from "next/link";
 
 const fleetsData = [
   {
@@ -14,6 +15,7 @@ const fleetsData = [
     name: "Toyota Etios",
     seats: "4+1",
     rate: "12-13",
+    textMessage: "I'm looking to hire Toyota Etios. Please share the details.",
   },
   {
     id: 2,
@@ -21,6 +23,7 @@ const fleetsData = [
     name: "Swift Dzire",
     seats: "4+1",
     rate: "12-13",
+    textMessage: "I'm looking to hire Swift Dzire. Please share the details.",
   },
   {
     id: 3,
@@ -28,6 +31,7 @@ const fleetsData = [
     name: "Toyota Innova",
     seats: "6+1",
     rate: "21-22",
+    textMessage: "I'm looking to hire Toyota Innova. Please share the details.",
   },
   {
     id: 4,
@@ -35,6 +39,7 @@ const fleetsData = [
     name: "Innova Crysta",
     seats: "6+1",
     rate: "21-22",
+    textMessage: "I'm looking to hire Innova Crysta. Please share the details.",
   },
   {
     id: 5,
@@ -42,6 +47,7 @@ const fleetsData = [
     name: "Ertiga",
     seats: "6+1",
     rate: "15-16",
+    textMessage: "I'm looking to hire Ertiga. Please share the details.",
   },
   {
     id: 6,
@@ -49,6 +55,7 @@ const fleetsData = [
     name: "Kia Carens",
     seats: "6+1",
     rate: "18",
+    textMessage: "I'm looking to hire Kia Carens. Please share the details.",
   },
   {
     id: 7,
@@ -56,6 +63,7 @@ const fleetsData = [
     name: "Mini Bus",
     seats: "20",
     rate: "35+",
+    textMessage: "I'm looking to hire Mini Bus. Please share the details.",
   },
   {
     id: 8,
@@ -63,6 +71,8 @@ const fleetsData = [
     name: "Tempo Traveller",
     seats: "13/17",
     rate: "25+",
+    textMessage:
+      "I'm looking to hire Tempo Traveller. Please share the details.",
   },
 ];
 
@@ -90,9 +100,12 @@ const OurFleets = () => {
                 {fleet.name} ({fleet.seats} Seater)
               </p>
               <div className="flex items-center justify-between">
-                <button className="border hover:bg-buttonPrimary hover:text-buttonText border-buttonPrimary text-buttonPrimary px-4 py-2 rounded-xl w-40 font-semibold">
+                <Link
+                  href={`https://wa.me/919076182397?text=${fleet.textMessage}`}
+                  className="border hover:bg-buttonPrimary hover:text-buttonText border-buttonPrimary text-buttonPrimary px-4 py-2 rounded-xl w-40 text-center font-semibold"
+                >
                   Book Now
-                </button>
+                </Link>
                 <p className="text-lg font-semibold">₹{fleet.rate}/km</p>
               </div>
             </div>
@@ -118,9 +131,12 @@ const OurFleets = () => {
                 {fleet.name} ({fleet.seats} Seater)
               </p>
               <div className="flex items-center justify-between">
-                <button className="border hover:bg-buttonPrimary hover:text-buttonText border-buttonPrimary text-buttonPrimary px-4 py-2 rounded-xl w-40 font-semibold">
+                <Link
+                  href={`https://wa.me/919076182397?text=${fleet.textMessage}`}
+                  className="border hover:bg-buttonPrimary hover:text-buttonText border-buttonPrimary text-buttonPrimary px-4 py-2 rounded-xl w-40 font-semibold text-center"
+                >
                   Book Now
-                </button>
+                </Link>
                 <p className="text-lg font-semibold">₹{fleet.rate}/km</p>
               </div>
             </div>
